05e96ef784cb2201255cfd3d349916c1edac257d
GuitarGuyB <isaacben822@gmail.com>
2021-04-12 12:02:48 -0400

    Added header's middleware to prevent bugs when fetching

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use((req, res, next) => {
+  res.header({ "Access-Control-Allow-Origin": "*" })
+  res.header({
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  })
+  res.header({
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  })
+  if (req.method == "OPTIONS") {
+    res.sendStatus(200)
+  }
+  next()
+})
 
 let db = []
 
